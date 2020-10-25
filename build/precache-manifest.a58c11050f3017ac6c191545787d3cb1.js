a092476419ba662d44e0ae5429e2454febc14def
jordi-jove <jordijove@gmail.com>
2020-10-25 09:18:16 +0100

    first commit

---
+++ build/precache-manifest.a58c11050f3017ac6c191545787d3cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c8851a2218f1dfdaf8b",
+    "url": "/static/css/main.c65dc0f5.chunk.css"
+  },
+  {
+    "revision": "6c8851a2218f1dfdaf8b",
+    "url": "/static/js/main.1692277f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0294c919377ff6bb99e7",
+    "url": "/static/js/2.96ee561f.chunk.js"
+  },
+  {
+    "revision": "2e7c36fdade606933326154b79b23378",
+    "url": "/index.html"
+  }
+];
